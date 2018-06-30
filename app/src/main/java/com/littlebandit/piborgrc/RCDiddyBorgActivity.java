874c36672bc0b9452617fd335758960053ae7218
jaianper <andres.rodas1990@gmail.com>
2018-06-29 22:40:12 -0500

    - Initial commit - Video stream from PiCamera to the mobile device using GStreamer.

---
+++ app/src/main/java/com/littlebandit/piborgrc/RCDiddyBorgActivity.java
@@ -0,0 +1,59 @@
+package com.littlebandit.piborgrc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.littlebandit.piborgrc.camstream.PICamStream;
+//import com.xd.racast.networkservice.WiFiDirectManager;
+
+public class RCDiddyBorgActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //WiFiDirectManager.newInstance(this).initializeService();
+
+        Button btnCameraStream = (Button)findViewById(R.id.btnPICameraStream);
+        btnCameraStream.setOnClickListener(this);
+
+        Button btnRemoteControl = (Button)findViewById(R.id.btnRemoteControl);
+        btnRemoteControl.setOnClickListener(this);
+    }
+
+    /* register the broadcast receiver with the intent values to be matched */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //WiFiDirectManager.getInstance().registerReceiver();
+        //WiFiDirectManager.getInstance().createGroup();
+    }
+
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //WiFiDirectManager.getInstance().unregisterReceiver();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.btnPICameraStream:
+            {
+                Intent intent = new Intent(RCDiddyBorgActivity.this, PICamStream.class);
+                startActivity(intent);
+            }break;
+            case R.id.btnRemoteControl:
+            {
+                //Intent intent = new Intent(RCDiddyBorgActivity.this, Other.class);
+                //startActivity(intent);
+            }break;
+        }
+    }
+}
---
+++ app/src/main/java/com/littlebandit/piborgrc/remotecontrol/RCDiddyBorg.java
@@ -0,0 +1,10 @@
+package com.littlebandit.piborgrc.remotecontrol;
+
+/**
+ * @author J414NP3R
+ */
+
+public class RCDiddyBorg
+{
+
+}
